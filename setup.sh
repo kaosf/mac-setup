eb5ca69a13d556c099d64ff5fe631fb746bfc7a1
ka <ka.kaosf@gmail.com>
2013-04-28 16:57:38 +0900

    fix .zshrc to .zshenv

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ git clone https://github.com/mxcl/homebrew --depth 1
 ln -s $HOME/local/bin/homebrew/bin/brew
 
 # GCC
-echo 'export PATH=$HOME/local/bin/homebrew/Library/ENV/4.3:$PATH' >> $HOME/.zshrc # or .bashrc
+echo 'export PATH=$HOME/local/bin/homebrew/Library/ENV/4.3:$PATH' >> $HOME/.zshenv # or .bashrc
 exec $SHELL
 
 # coreutils
@@ -35,3 +35,10 @@ brew install openssl curl-ca-bundle readline
 cp \
  $HOME/local/bin/homebrew/Cellar/curl-ca-bundle/1.87/share/ca-bundle.crt \
  $HOME/local/etc/openssl/cert.pem
+
+brew install rbenv
+echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv # or .bashrc
+exec $SHELL
+brew install ruby-build
+
+RUBY_CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --with-openssl-dir=`brew --prefix openssl`" rbenv install 2.0.0-p0
