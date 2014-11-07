6adbf729590fa708d8e460298842558b303fcd23
ka <ka.kaosf@gmail.com>
2014-11-07 20:49:55 +0900

    Fix Homebrew install memo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,12 @@
 #! /bin/sh
 
-# ref. https://gist.github.com/kaosf/5471662
-echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zshenv # or .profile
-exec $SHELL
-mkdir -p $HOME/local/bin
-cd $HOME/local/bin
-git clone https://github.com/mxcl/homebrew --depth 1
-ln -s $HOME/local/bin/homebrew/bin/brew
+# Install Xcode
 
-# GCC
-echo 'export PATH=$HOME/local/bin/homebrew/Library/ENV/4.3:$PATH' >> $HOME/.zshenv # or .profile
-exec $SHELL
+# Install "Command Line Tools for Xcode"
+# Open Xcode > Xcode > Open Developer Tool > More Developer Tools...
+
+# Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # coreutils
 brew install coreutils
